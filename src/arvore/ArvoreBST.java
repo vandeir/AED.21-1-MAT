f88d90a1c8ef3f26fce6141477e05db2731838b4
Marcel <marcel@furb.br>
2021-04-28 10:27:08 -0300

    ArvoreBST - buscar

--- src/arvore/ArvoreBST.java
+++ src/arvore/ArvoreBST.java
@@ -5,13 +5,18 @@ public class ArvoreBST<T extends Comparable> extends ArvoreBinariaAbstract<T> {
 	public void inserir(T info) {
 		if (this.vazia()) {
 			this.setRaiz(new NoArvoreBST<>(info));
-		}
-		else {
-			this.getRaiz().inserir(info);
+		} else {
+			((NoArvoreBST<T>)this.getRaiz()).inserir(info);
 		}
 	}
-	
+
 	public NoArvoreBST<T> buscar(T procurado) {
-		return null;
+		if (this.vazia()) {
+			return null;
+		} else {
+			return ((NoArvoreBST<T>)this.getRaiz()).buscar(procurado);
+		}
 	}
+
+
 }
--- src/arvore/NoArvoreBST.java
+++ src/arvore/NoArvoreBST.java
@@ -6,4 +6,15 @@ public class NoArvoreBST<T extends Comparable> extends NoArvoreBinaria<T> {
 		super(info);
 	}
 
+	public NoArvoreBST<T> buscar(T procurado) {
+		if (procurado.equals(this.getInfo())) {
+			return this;
+		} else {
+			if (procurado.compareTo(this.getInfo()) < 0) {
+				return ((NoArvoreBST<T>)this.getEsq()).buscar(procurado);
+			} else {
+				return ((NoArvoreBST<T>)this.getDir()).buscar(procurado);
+			}
+		}
+	}
 }
